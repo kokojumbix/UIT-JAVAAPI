f2bc479ffecbceea5c4b6f597604a7b6b53c62f4
Mark Chorniy <12marik12@gmail.com>
2023-11-20 23:26:01 +0200

    Initial commit

---
+++ src/test/java/com/apiusers/demo/Uit2labApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiusers.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uit2labApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
