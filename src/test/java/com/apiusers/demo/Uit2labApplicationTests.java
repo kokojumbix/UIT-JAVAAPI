cd84b9161238e66eb23b93129bd78cf65ed1a104
Mark Chorniy <12marik12@gmail.com>
2023-11-22 21:13:36 +0200

    Added new page and tests

--- src/test/java/com/apiusers/demo/Uit2labApplicationTests.java
+++ src/test/java/com/apiusers/demo/Uit2labApplicationTests.java
@@ -1,13 +1,43 @@
 package com.apiusers.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Uit2labApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private MainController controller;
+
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
-	void contextLoads() {
+	void controllerAccess() throws Exception {
+		assertThat(controller).isNotNull();
 	}
+
+	@Test
+	void contextLoads()
+		throws Exception {
+			assertThat( this.restTemplate.getForObject("http://localhost:" + port + "/user/", String.class).toString().contains("localhorse.space")).isTrue() ;
+
+	}
+
+
+
 
 }
